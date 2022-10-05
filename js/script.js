e8d55d74c549b9d1bf84ec62ed6ce25edf39c598
Noel <horizon33noela@gmail.com>
2022-10-05 00:31:37 -0600

    modified all files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-const a =50;
+
+$(() => {
+  var scrollLink = $(".nav-link-scroll");
+
+  //smooth scrolling
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $("html").animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+        
+      },
+      2000
+    );
+  });
+});
