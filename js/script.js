a8dc0c3f6170dc2c85188135da22c3b04b7502e0
Noel <horizon33noela@gmail.com>
2022-12-06 18:13:03 -0700

    modified files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+        document.querySelector('#github').onclick= MyFunction
+        document.querySelector('#lkdn').onclick=MyFunction
+        document.querySelector('#learn').onclick=MyFunction
+}
+
+function MyFunction(){
+   alert('Work In Progress ...')
+}
