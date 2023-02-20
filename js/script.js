18766044d142d197c6a58a7e053e792d8ff851ce
Noel <115832045+ntesfe8@users.noreply.github.com>
2023-02-20 06:43:33 -0700

    files modified

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-window.onload = function(){
-        document.querySelector('#github').onclick= MyFunction
-        document.querySelector('#lkdn').onclick=MyFunction
-        document.querySelector('#learn').onclick=MyFunction
-}
+window.onload = function () {
+  document.querySelector("#github").onclick = MyFunction;
+  document.querySelector("#lkdn").onclick = MyFunction;
+  document.querySelector("#learn").onclick = MyFunction;
 
-function MyFunction(){
-   alert('Work In Progress ...')
+  //scrolling the window
+  document.querySelectorAll(".navigation-list li").forEach(function (listItem) {
+    listItem.addEventListener("click", function () {
+      let topPosition =
+        document.getElementById(listItem.dataset.page).offsetTop - 60;
+      window.scrollTo({
+        top: topPosition,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  });
+};
+
+function MyFunction() {
+  alert("Work In Progress ...");
 }
