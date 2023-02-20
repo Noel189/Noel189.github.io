c5f5041031c0417baf037cf566a82331ab354085
Noel <115832045+ntesfe8@users.noreply.github.com>
2023-02-20 07:45:53 -0700

    modified

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   document.querySelector("#lkdn").onclick = MyFunction;
   document.querySelector("#learn").onclick = MyFunction;
 
-  //scrolling the window
+  //scrolling to  a particular section of the window
   document.querySelectorAll(".navigation-list li").forEach(function (listItem) {
     listItem.addEventListener("click", function () {
       let topPosition = document.querySelector("#"+this.getAttribute('data-id')).offsetTop - 60;
