1e50ef81ec09d6d121557d447ba90544b12b6609
Noel <horizon33noela@gmail.com>
2024-04-30 15:29:41 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 window.onload = function () {
-  document.querySelector("#github").onclick = MyFunction;
-  document.querySelector("#lkdn").onclick = MyFunction;
-  document.querySelector("#learn").onclick = MyFunction;
+  document.querySelector('#github').onclick = MyFunction;
+  document.querySelector('#lkdn').onclick = MyFunction;
+  document.querySelector('#learn').onclick = MyFunction;
 
   //scrolling to  a particular section of the window
-  document.querySelectorAll(".navigation-list li").forEach(function (listItem) {
-    listItem.addEventListener("click", function () {
-      let topPosition = document.querySelector("#"+this.getAttribute('data-id')).offsetTop - 60;
+  document.querySelectorAll('.navigation-list li').forEach(function (listItem) {
+    listItem.addEventListener('click', function () {
+      document.querySelector('.navigation-pane').classList.add('show');
+      let topPosition =
+        document.querySelector('#' + this.getAttribute('data-id')).offsetTop -
+        60;
       window.scrollTo({
         top: topPosition,
         left: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
+      // Hide the navigation menu after click
+      document.querySelector('.navigation-pane').style.dipslay = 'none';
     });
   });
 };
 
 function MyFunction() {
-  alert("Work In Progress ...");
+  alert('Work In Progress ...');
 }
